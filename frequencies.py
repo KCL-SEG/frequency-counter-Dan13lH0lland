12c0971bcb77f68d31ccc04084b43a8c42975cc9
Daniel Holland <daniel.1.holland@kcl.ac.uk>
2022-10-01 16:51:02 +0100

    Adding update to convert ints to strings

--- frequencies.py
+++ frequencies.py
@@ -1,13 +1,20 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
+def frequencies(*items):
     frequencies = {}
     print(items)
-    for item in items:
+    newItems = []
+    for z in items:
+        if isinstance(z, int):
+            newItems.append(str(z))
+        else:
+            newItems.append(z)
+    for item in newItems:
         counter = 0
-        for x in items:
-            if x == item:
+        for x in newItems:
+            y = str(x)
+            if y == item:
                 counter += 1
         frequencies[item] = counter
     print(frequencies)
