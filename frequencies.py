91fab00cb374390c7cc49a18793a0e69dfa33a29
Daniel Holland <daniel.1.holland@kcl.ac.uk>
2022-10-01 16:23:03 +0100

    Adding frequencies of how many times 'x' appears in items

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
+def frequencies(*items):
     frequencies = {}
+    print(items)
+    for item in items:
+        counter = 0
+        for x in items:
+            if x == item:
+                counter += 1
+        frequencies[item] = counter
+    print(frequencies)
     # Your code goes here
     return frequencies
